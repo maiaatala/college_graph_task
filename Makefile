e7422617f5ffd034e68c7d74cd5344c4ffb67207
maiaatala <ana.atala@unemat.br>
2023-06-18 21:38:46 -0400

    [feat]: recursive minimum path and regular and aciclic graph

--- Makefile
+++ Makefile
@@ -7,6 +7,8 @@ OBJECT = main.o
 INPUT_FILE = arq_grafo.txt
 30_FILE = grafo_15_30.txt
 100_FILE = grafo_15_100.txt
+REGULAR_FILE = grafo_regular.txt
+NCICLICO_FILE = grafo_nciclico.txt
 OUTPUT_FILE = output.txt
 
 final: $(OBJECT)
@@ -20,16 +22,17 @@ $(OBJECT): $(SOURCE)
 	$(CC) $(CFLAGS) $(OBJECT) -o programa
 	./programa $(30_FILE) $(OUTPUT_FILE)
 
-$(OBJECT): $(SOURCE)
-	$(CC) $(CFLAGS) -c $(SOURCE) -o $(OBJECT)
-
 100: $(OBJECT)
 	$(CC) $(CFLAGS) $(OBJECT) -o programa
 	./programa $(100_FILE) $(OUTPUT_FILE)
 	
-$(OBJECT): $(SOURCE)
-	$(CC) $(CFLAGS) -c $(SOURCE) -o $(OBJECT)
+REG: $(OBJECT)
+	$(CC) $(CFLAGS) $(OBJECT) -o programa
+	./programa $(REGULAR_FILE) $(OUTPUT_FILE)
 
-
+NCI: $(OBJECT)
+	$(CC) $(CFLAGS) $(OBJECT) -o programa
+	./programa $(NCICLICO_FILE) $(OUTPUT_FILE)
+	
 clean:
 	rm -f programa $(OBJECT) $(OUTPUT_FILE)
--- main.c
+++ main.c
@@ -30,7 +30,7 @@ void lerGrafo(FILE *arquivo, int *numVertices, int *numArestas, int **vertices, 
     }
 }
 
-void escreverGrafo(FILE *arquivo, int numVertices, int numArestas, int vertices[], Aresta arestas[]) {
+void printGraph(FILE *arquivo, int numVertices, int numArestas, int vertices[], Aresta arestas[]) {
     fprintf(arquivo, "Grafo\n");
     fprintf(arquivo, "V: ");
     for (int i = 0; i < numVertices; i++) {
@@ -50,44 +50,40 @@ void escreverGrafo(FILE *arquivo, int numVertices, int numArestas, int vertices[
     }
     
     // Check if there are any unvisited vertices
-    for (int i = 0; i < numVertices; i++) {
-        if (!visited[i]) {
-            fprintf(arquivo, "(%d) ", vertices[i]);
-        }
-    }
+    // for (int i = 0; i < numVertices; i++) {
+    //     if (!visited[i]) {
+    //         fprintf(arquivo, "(%d) ", vertices[i]);
+    //     }
+    // }
     
     fprintf(arquivo, "\n");
     free(visited);
 }
 
 int verificarRegularidade(int numVertices, Aresta arestas[], int numArestas) {
-    int grauPrimeiroVertice = 0;
-    int grauAtual;
+    int *graus = (int *)calloc(numVertices, sizeof(int));
 
-    // Contagem do grau do primeiro vértice
+    // Contagem dos graus dos vértices
     for (int i = 0; i < numArestas; i++) {
-        if (arestas[i].origem == 1) {
-            grauPrimeiroVertice++;
-        }
+        int origem = arestas[i].origem - 1;
+        int destino = arestas[i].destino - 1;
+        graus[origem]++;
+        graus[destino]++;
     }
 
-    // Verificação do grau dos demais vértices
-    for (int i = 2; i <= numVertices; i++) {
-        grauAtual = 0;
-
-        for (int j = 0; j < numArestas; j++) {
-            if (arestas[j].origem == i || arestas[j].destino == i) {
-                grauAtual++;
-            }
-        }
-
-        if (grauAtual != grauPrimeiroVertice) {
+    // Verificação se todos os vértices têm o mesmo grau
+    int grauPrimeiroVertice = graus[0];
+    for (int i = 1; i < numVertices; i++) {
+        if (graus[i] != grauPrimeiroVertice) {
+            free(graus);
             return 0; // Grafo é irregular
         }
     }
 
+    free(graus);
     return 1; // Grafo é regular
 }
+
 
 int verificarCicloRecursivo(int vertice, int visitados[], int pai, Aresta arestas[], int numArestas) {
     visitados[vertice] = 1;
@@ -159,6 +155,17 @@ int encontrarMaiorVertice(int numVertices, int *vertices) {
     return maior;
 }
 
+void imprimirCaminhoMinimoRecursivo(FILE *saida, int *predecessores, int origem, int destino) {
+    if (origem == destino) {
+        fprintf(saida, "%d", origem);
+    } else if (predecessores[destino - 1] == -1) {
+        fprintf(saida, "Não existe caminho entre a origem e o destino.\n");
+    } else {
+        imprimirCaminhoMinimoRecursivo(saida, predecessores, origem, predecessores[destino - 1] + 1);
+        fprintf(saida, "-%d", destino);
+    }
+}
+
 void imprimirCaminhoMinimo(FILE *saida, int numVertices, Aresta *arestas, int numArestas, int origem, int destino) {
     // Vetor de distâncias e vetor de predecessores
     int *distancias = (int *)malloc(numVertices * sizeof(int));
@@ -208,34 +215,11 @@ void imprimirCaminhoMinimo(FILE *saida, int numVertices, Aresta *arestas, int nu
             }
         }
     }
+    fprintf(saida, "Caminho mínimo: ");
+    // Imprimir o caminho mínimo
+    imprimirCaminhoMinimoRecursivo(saida, predecessores, origem, destino);
 
-    // Verificar se há um caminho mínimo até o destino
-    if (predecessores[destino - 1] == -1) {
-        fprintf(saida, "Caminho mínimo: Não existe caminho entre a origem e o destino.\n");
-    } else {
-        // Construir o caminho mínimo
-        int *caminho = (int *)malloc(numVertices * sizeof(int));
-        int index = 0;
-        int atual = destino - 1;
-
-        while (atual != -1) {
-            caminho[index] = atual + 1;
-            index++;
-            atual = predecessores[atual];
-        }
-
-        // Imprimir o caminho mínimo
-        fprintf(saida, "Caminho mínimo: ");
-        for (int i = index - 1; i >= 0; i--) {
-            fprintf(saida, "%d", caminho[i]);
-            if (i != 0) {
-                fprintf(saida, "-");
-            }
-        }
-        fprintf(saida, "\n");
-
-        free(caminho);
-    }
+    fprintf(saida, "\n");
 
     free(distancias);
     free(predecessores);
@@ -270,7 +254,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    escreverGrafo(saida, numVertices, numArestas, vertices, arestas);
+    printGraph(saida, numVertices, numArestas, vertices, arestas);
+
     if (verificarRegularidade(numVertices, arestas, numArestas)) {
         fprintf(saida, "Regular\n");
     } else {
